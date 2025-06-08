92e4ea140e647c4ae50b32c389672cfb38cec09b
Fernando Barbosa de Souza <32278962+nandobs1980@users.noreply.github.com>
2025-06-08 16:37:19 -0300

    fix: adiciona main guard e tratamento de excecoes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,29 @@
-print("Hello, World!!!")
 import requests
 
-nome_pokemon = "pikachu"
-url = f"https://pokeapi.co/api/v2/pokemon/{nome_pokemon}"
 
-response = requests.get(url)
-if response.status_code == 200:
-    dados = response.json()
-    print(f"Nome: {dados['name']}")
-    print(f"ID: {dados['id']}")
-    print(f"Altura: {dados['height']}")
-    print(f"Peso: {dados['weight']}")
-    print("Habilidades:")
-    for habilidade in dados["abilities"]:
-        print(" -", habilidade["ability"]["name"])
-else:
-    print("Pokémon não encontrado!")
+def main():
+    print("Hello, World!!!")
+    nome_pokemon = "pikachu"
+    url = f"https://pokeapi.co/api/v2/pokemon/{nome_pokemon}"
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            dados = response.json()
+            print(f"Nome: {dados['name']}")
+            print(f"ID: {dados['id']}")
+            print(f"Altura: {dados['height']}")
+            print(f"Peso: {dados['weight']}")
+            print("Habilidades:")
+            for habilidade in dados["abilities"]:
+                print(" -", habilidade["ability"]["name"])
+        else:
+            print("Pokémon não encontrado!")
+    except requests.exceptions.RequestException:
+        print("Falha ao conectar-se à PokeAPI.")
+
+
+if __name__ == "__main__":
+    main()
 
 
